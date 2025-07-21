ce3d7989ed0f7c961541793bf65b32cd5da03fef
LucasVicentee <lh201612@gmail.com>
2025-07-21 17:27:32 -0300

    Implementando Getters e Setters na classe Aluno

--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -1,10 +1,56 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Aluno extends Pessoa {
 
+    private ArrayList<String> materias;
+    private double nota;
+    private int ra;
 
-
-    public Aluno(String nome, int idade, String cpf) {
+    public Aluno(String nome, int idade, String cpf, double nota, int ra) {
         super(nome, idade, cpf);
+        this.materias = new ArrayList<>();
+        this.nota = nota;
+        this.ra = ra;
+    }
+
+    public void adicionarMateria(String materias) {
+        this.materias.add(materias);
+    }
+
+    public void removerMateria(String materias) {
+        this.materias.remove(materias);
+    }
+
+    public ArrayList<String> getMateria() {
+        return materias;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("Informações do Aluno: " +
+                "\nNome: %s, " +
+                "\nIdade: %d, " +
+                "\nCPF: %s, " +
+                "\nNúmero do RA: %d", nome, idade, cpf, ra);
     }
 }
--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -16,15 +16,23 @@ public class Pessoa {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public int getIdade() {
         return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
     public String getCpf() {
         return cpf;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
