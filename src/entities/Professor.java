52cf981f7d06a990e6c76beff0a6ae1b92da50c1
LucasVicentee <lh201612@gmail.com>
2025-07-22 15:59:11 -0300

    Adicionando atributos, getters e setter para a classe professor

--- src/entities/Professor.java
+++ src/entities/Professor.java
@@ -6,12 +6,14 @@ public class Professor extends Pessoa {
 
     private ArrayList<String> materias;
     private int rf;
+    private double salario;
 
 
-    public Professor(String nome, int idade, String cpf, int rf) {
+    public Professor(String nome, int idade, String cpf, int rf, double salario) {
         super(nome, idade, cpf);
         this.materias = new ArrayList<>();
         this.rf = rf;
+        this.salario = salario;
     }
 
     public void AdicionarMateria(String materias) {
@@ -34,12 +36,21 @@ public class Professor extends Pessoa {
         this.rf = rf;
     }
 
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
     @Override
     public String toString() {
         return String.format("Informações do professor: " +
                 "\nNome: %s, " +
                 "\nIdade: %d, " +
                 "\nCPF: %s, " +
-                "\nNúmero do RF: %d", nome, idade, cpf, rf);
+                "\nNúmero do RF: %d" +
+                "\nSalário: %f", nome, idade, cpf, rf, salario);
     }
 }
